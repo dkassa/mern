6f7def5f2d6dffc1b93db89033f62c72aebdf761
desalegn kassa <desalegnashagrie@gmail.comgit config user.emailgit config user.emailgit config --global user.email desalegnashagrie@gmail.com>
2023-04-25 00:39:08 -0500

    build

--- frontend/src/pages/admin/AdminCreateProductPage.js
+++ frontend/src/pages/admin/AdminCreateProductPage.js
@@ -1,34 +1,43 @@
 import CreateProductPageComponent from "./components/CreateProductPageComponent";
 import axios from "axios";
-import {
-  uploadImagesApiRequest,
-  uploadImagesCloudinaryApiRequest,
-} from "./utils/utils";
-import { useSelector } from "react-redux";
-import { newCategory,deleteCategory, saveAttributeToCatDoc } from "../../redux/actions/categoryActions";
-import { useDispatch } from "react-redux";
 
 const createProductApiRequest = async (formInputs) => {
-  const { data } = await axios.post(`/api/products/admin`, { ...formInputs });
-  return data;
-};
+    const { data } = await axios.post(`/api/products/admin`, { ...formInputs });
+    return data;
+}
+
+const uploadImagesApiRequest = async (images, productId) => {
+    const formData = new FormData();
+    Array.from(images).forEach(image => {
+        formData.append("images", image);
+    })
+    await axios.post("/api/products/admin/upload?productId=" + productId, formData);
+}
+
+const uploadImagesCloudinaryApiRequest = (images,productId) => {
+    const url = "https://api.cloudinary.com/v1_1/dq8rvolnk/image/upload";
+    const formData = new FormData();
+    for (let i = 0; i < images.length; i++) {
+        let file = images[i];
+        formData.append("file", file);
+        formData.append("upload_preset", "acsijtdn");
+        fetch(url, {
+            method: "POST",
+            body: formData,
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            axios.post("/api/products/admin/upload?cloudinary=true&productId=" + productId, data);
+        })
+    }
+}
 
 const AdminCreateProductPage = () => {
-  const { categories } = useSelector((state) => state.getCategories);
-  const dispatch = useDispatch();
-
-  return (
-    <CreateProductPageComponent
-      createProductApiRequest={createProductApiRequest}
-      uploadImagesApiRequest={uploadImagesApiRequest}
-      uploadImagesCloudinaryApiRequest={uploadImagesCloudinaryApiRequest}
-      categories={categories}
-      reduxDispatch={dispatch}
-      newCategory={newCategory}
-      deleteCategory={deleteCategory}
-      saveAttributeToCatDoc={saveAttributeToCatDoc}
-    />
-  );
+  
+  return <CreateProductPageComponent createProductApiRequest={createProductApiRequest} uploadImagesApiRequest={uploadImagesApiRequest} uploadImagesCloudinaryApiRequest={uploadImagesCloudinaryApiRequest} />;
 };
 
 export default AdminCreateProductPage;
+
