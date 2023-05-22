f6348de07c2be93cd05f77d40dbe0025e18f0463
desalegn kassa <desalegnashagrie@gmail.comgit config user.emailgit config user.emailgit config --global user.email desalegnashagrie@gmail.com>
2023-05-22 00:32:15 -0500

    fixed usenavigate

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -12,10 +12,12 @@ export const setReduxUserState = (userCreated) => (dispatch) => {
 export const logout = () => (dispatch) => {
     
 const navigate=useNavigate()
-    navigate("/login");
+
     axios.get('/api/logout')
     localStorage.removeItem("userInfo");
     sessionStorage.removeItem("userInfo");
     localStorage.removeItem("cart");
     dispatch({ type: LOGOUT_USER })
+
+    navigate('/login')
 }
